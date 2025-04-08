871795244dcce7c21cc011081cf065f09bf7c923
LuisCReyes <lcreyes@uamv.edu.ni>
2025-04-08 13:35:04 -0600

    Ejercicio 8

---
+++ Sesion 2/Ejercicio8.py
@@ -0,0 +1,11 @@
+"""Observa las siguientes variables y escribe qué tipo de convención de nomenclatura
+se está utilizando (camelCase, PascalCase, snake_case, SCREAMING_SNAKE_CASE,
+kebab-case).
+a) totalAmount
+b) TotalAmount
+c) total_amount
+d) TOTAL_AMOUNT
+e) total-amount
+"""
+
+print("totalAmount: es camelCase,"," TotalAmount: es PascalCase,"," total_amount: es snake_case,"," TOTAL_AMOUNT: es SCREAMING_SNAKE_CASE,"," total-amount: es kebab-case")
