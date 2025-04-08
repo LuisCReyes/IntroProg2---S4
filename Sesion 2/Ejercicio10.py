acd0314b89167fa31e180af7d9436685a16b266e
LuisCReyes <lcreyes@uamv.edu.ni>
2025-04-08 13:36:28 -0600

    Ejercicio10

---
+++ Sesion 2/Ejercicio10.py
@@ -0,0 +1,19 @@
+"""""Declara nombres de variables para los siguientes casos, aplicando la convención
+indicada.
+1. Nombre completo del estudiante – camelCase
+2. Número de matrícula – snake_case
+3. Total de ventas del mes – PascalCase
+4. Límite máximo de intentos – SCREAMING_SNAKE_CASE
+5. Cantidad de productos – camelCase"""
+
+nombreCompleto = "Luis Reyes"
+número_de_matrícula = 5547861238
+TotalDeVentasDelMes = 24
+LIMITE_MÁXIMO_DE_INTENTOS = 3
+cantidadDeProductos = 25
+
+print(f"Nombre: {nombreCompleto}")
+print(f"Número de matrícula: {número_de_matrícula}")
+print(f"Total de ventas del mes: {TotalDeVentasDelMes}")
+print(f"Límite máximo de intentos: {LIMITE_MÁXIMO_DE_INTENTOS}")
+print(f"Cantidad de productos: {cantidadDeProductos}")
