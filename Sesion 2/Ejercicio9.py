c98358084867d55006166134db8741e11a5a8189
LuisCReyes <lcreyes@uamv.edu.ni>
2025-04-08 13:35:09 -0600

    Ejercicio 9

---
+++ Sesion 2/Ejercicio9.py
@@ -0,0 +1,10 @@
+""""Los siguientes nombres de variables están mal escritos o no siguen una convención
+clara. Corrígelos utilizando camelCase o snake_case, según se indique.
+Versión original | Convención a usar
+a) nombrepersona → camelCase
+b) EdadPersona → snake_case
+c) TotalPrecio → camelCase
+d) numero-de-telefono → snake_case
+e) DireccionEmail → camelCase"""
+
+print("a) nombrePersona, b) edad_persona, c) totalPrecio, d) numero_de_telefono, direcciónEmail")
