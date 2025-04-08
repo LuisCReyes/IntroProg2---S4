dcd9e2add1009fd9a91b35ff34ace881f6a39026
LuisCReyes <lcreyes@uamv.edu.ni>
2025-04-08 08:41:21 -0600

    Actividades python 8/04/25

---
+++ ejercicio1.py
@@ -0,0 +1,14 @@
+#declarar variables
+nommbre_completo = "Juan Cristo Carvallo Herrera"
+edad = 17
+altura = 1.72
+soltero = True
+
+print(f"Nombre completo: {nommbre_completo}")
+print(f"Tipo de dato:{type(nommbre_completo)}")
+print(f"Edad: {edad}")
+print(f"Tipo de dato: {type(edad)}")
+print(f"Altura: {altura}")
+print(f"Tipo de dato: {type(altura)}")
+print(f"Soltero: {soltero}")
+print(f"Tipo de dato: {type(soltero)}")
