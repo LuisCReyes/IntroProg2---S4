1880d45d9b30294233b3708d314d60227e6b0511
LuisCReyes <lcreyes@uamv.edu.ni>
2025-04-08 13:36:52 -0600

    Ejercicio12

---
+++ Sesion 2/Ejercicio12.py
@@ -0,0 +1,12 @@
+prestamo = 10000
+tasa_de_interes = 0.27
+
+interes_prest = prestamo * tasa_de_interes
+
+print("+" * 50)
+print("Bienvenido a tu banca digital 😊")
+print(f"Su prestamo es de ${prestamo}")
+print(f"Con un intéres del {tasa_de_interes} %")
+print(f"Su prestamo sumado con el intéres será del: ${interes_prest}")
+print("Gracias por usar nuestros servicios 😊😊")
+print("+" * 50)
