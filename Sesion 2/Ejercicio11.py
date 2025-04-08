69c8ef26f7f0ba3ef4afac7a26e93da2eb51fd9b
LuisCReyes <lcreyes@uamv.edu.ni>
2025-04-08 13:36:41 -0600

    Ejercicio11

---
+++ Sesion 2/Ejercicio11.py
@@ -0,0 +1,12 @@
+
+a = "La edad del usuario"
+b = "El precio total del carrito de compras"
+c = "El estado de inicio de sesión (activo o inactivo)"
+d = "El nombre del curso actual"
+e = "La temperatura promedio registrada"
+
+print(f"{a} edad_usuario")
+print(f"{b} precio_total_carrito_compra")
+print(f"{c} estado_inicio_sesión")
+print(f"{d} curso_actual")
+print(f"{e} temperatura_promedio")
